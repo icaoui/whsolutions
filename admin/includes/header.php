8038bedfa32b50c4adf50ff73e0245a7cba21cc2
Issam CAOUI <icaoui@btechnologie.com>
2026-04-23 21:17:56 +0100

    Fix: admin header.php require paths (/../ -> /../../)

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../config/config.php';
-require_once __DIR__ . '/../includes/functions.php';
+require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../../includes/functions.php';
 
 if (!isset($_SESSION['admin_id'])) {
     header('Location: login.php');
